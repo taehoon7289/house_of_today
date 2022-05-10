d16d125c0645976e090242242c3c5c017164004c
taehoon-coder <taeh@noah-nomad.com>
2022-05-10 11:13:28 +0900

    feat: my_app_bar_controller.dart 추가
    - my_app_bar_controller.dart 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:house_of_today/controller/my_app_bar_controller.dart';
 import 'package:house_of_today/page/my_home.dart';
 
 void main() => runApp(MyMain());
@@ -9,6 +10,7 @@ class MyMain extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Get.put(MyAppBarController());
     return GetMaterialApp(
       title: '오늘의집 clone coding',
       debugShowCheckedModeBanner: false,
